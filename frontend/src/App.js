93f870686666b6f09205d7125bb05f6c997a620a
Svetlana <skarga0909@gmail.com>
2022-08-19 17:41:16 +0300

    lesson_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import AuthorList from "./components/Author";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends  React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/authors/').then(response =>{
+
+       this.setState(
+        {
+          'authors':response.data
+        }
+    )
+    }).catch(error => console.log(error))
+    // const authors = [
+    //   {
+    //     'first_name':'Фёдор',
+    //     'last_name':'Достаевский',
+    //     'birthday_year':1821,
+    //   },
+    //   {
+    //     'first_name':'Александр',
+    //     'last_name':'Грин',
+    //     'birthday_year':1880,
+    //   }
+    //
+    // ]
+
+  }
+
+
+  render() {
+    return (
+      <div>
+         <AuthorList authors={this.state.authors}/>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+
+
+
--- library/settings.py
+++ library/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+]
 
 # Application definition
 
@@ -40,12 +44,15 @@ INSTALLED_APPS = [
 
     #main
     'rest_framework',
-    'authors'
+    'authors',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
